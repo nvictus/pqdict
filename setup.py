d06047f2398c7034e721f4073f3aca3d2aea66af
Nezar Abdennur <nabdennur@gmail.com>
2013-07-22 12:11:03 -0400

    Second beta release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os.path
 README = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 setup(name='pqdict',
-      version='0.1',
+      version='0.2',
       description='An indexed priority queue implementation with a dictionary interface',
       author='Nezar Abdennur',
       author_email='nabdennur@gmail.com',
