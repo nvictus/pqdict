05b4c7560d9dad3204cfa3a18066f19fba36daa1
Nezar Abdennur <nabdennur@gmail.com>
2015-08-31 13:02:22 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,59 @@
-from distutils.core import setup
-import os.path
-import pqdict
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import re
 
-README = os.path.join(os.path.dirname(__file__), 'README.rst')
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name='pqdict',
-      version='%d.%d' % pqdict.__version__[:-1],
-      description='A dictionary-like indexed priority queue',
-      author='Nezar Abdennur',
-      author_email='nabdennur@gmail.com',
-      url='https://github.com/nvictus/priority-queue-dictionary',
-      license='MIT',
-      py_modules=['pqdict'],
-      long_description=open(README).read(),
-      keywords='dict priority queue heap scheduler data structures',
-      classifiers=["Programming Language :: Python",
-                   "Programming Language :: Python :: 2",
-                   "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3",
-                   "Programming Language :: Python :: 3.2",
-                   "Programming Language :: Python :: 3.3",
-                   "Programming Language :: Python :: Implementation :: CPython",
-                   "Programming Language :: Python :: Implementation :: PyPy",
-                   "Operating System :: OS Independent",
-                   "License :: OSI Approved :: MIT License",
-                   "Topic :: Software Development",
-                   "Topic :: Software Development :: Libraries",
-                   "Topic :: Software Development :: Libraries :: Python Modules"]
-     )
+
+classifiers = """\
+    Development Status :: 4 - Beta
+    Operating System :: OS Independent
+    License :: OSI Approved :: MIT License
+    Programming Language :: Python
+    Programming Language :: Python :: 2
+    Programming Language :: Python :: 2.7
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3.2
+    Programming Language :: Python :: 3.3
+    Programming Language :: Python :: 3.4
+    Programming Language :: Python :: 3.5
+    Programming Language :: Python :: Implementation :: CPython
+    Programming Language :: Python :: Implementation :: PyPy
+"""
+
+
+def _read(*parts, **kwargs):
+    filepath = os.path.join(os.path.dirname(__file__), *parts)
+    with io.open(filepath) as fh:
+          text = io.open(fh, encoding=kwargs.pop('encoding', 'utf-8')).read()
+    return text
+
+
+def get_version():
+    regex = re.compile(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]')
+    return regex.search(_read('pqdict', '__init__.py')).group(1)
+
+
+def get_long_description():
+    return _read('README.rst')
+
+
+setup(
+    name='pqdict',
+    version=get_version(),
+    license='MIT',
+    description='A dictionary-like indexed priority queue',
+    long_description=get_long_description(),
+    keywords=['dict', 'priority queue', 'heap', 'scheduler', 'data structures'],
+    author='Nezar Abdennur',
+    author_email='nabdennur@gmail.com',
+    url='https://github.com/nvictus/priority-queue-dictionary',
+    packages=['pqdict'],
+    zip_safe=False,
+    classifiers=[s.strip() for s in classifiers.split('\n') if s]
+)
