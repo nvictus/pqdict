5ae35723389c855f3751e09e6d61cbfed836bda7
Eugene Prilepin <esp.home@gmail.com>
2020-08-20 23:11:34 +0300

    fix test for python < 3.6

--- tests/test_pqdict.py
+++ tests/test_pqdict.py
@@ -2,6 +2,7 @@
 from datetime import datetime, timedelta
 from itertools import combinations
 import operator
+import sys
 import random
 
 import pytest
@@ -12,7 +13,7 @@ from pqdict import pqdict, minpq, maxpq, nlargest, nsmallest
 sample_keys = ["A", "B", "C", "D", "E", "F", "G"]
 sample_values = [5, 8, 7, 3, 9, 12, 1]
 sample_tuple_values = [
-    ("a", 5), ("b", 8), (None, 7), ("d", 3), ("e", 9), ("f", 12), ("g", 1)
+    ("a", 5), ("b", 8), (None, 7), (2.5, 3), ("e", 9), ("f", 12), ("g", 1)
 ]
 sample_items = list(zip(sample_keys, sample_values))
 sample_tuple_items = list(zip(sample_keys, sample_tuple_values))
@@ -517,10 +518,11 @@ def test_nbest():
 
 
 def test_nbest_key():
-    with pytest.raises(TypeError, match="'<' not supported"):
-        nlargest(3, dict(sample_tuple_items))
-    with pytest.raises(TypeError, match="'>' not supported"):
-        nsmallest(3, dict(sample_tuple_items))
+    if sys.version_info.major > 2:
+        with pytest.raises(TypeError):
+            nlargest(3, dict(sample_tuple_items))
+        with pytest.raises(TypeError):
+            nsmallest(3, dict(sample_tuple_items))
 
     top3 = nlargest(3, dict(sample_tuple_items), key=operator.itemgetter(1))
     assert list(top3) == ["F", "E", "B"]
